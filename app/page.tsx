02cf505fa916ba142372d4f84adf4c50cef47ed8
Atul <pawadeatul3@gmail.com>
2024-08-01 14:11:19 +0530

    system backup

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import HeroSection from "@/components/Home/HeroSection";
 import ThoughtSlider from "@/components/Home/ThoughtSlider";
 import TypedSection from "@/components/Home/TypedSection";
+import Image from "next/image";
 
 export default function Home() {
   const beforeText = "We are a Trusted Team"
@@ -14,13 +15,29 @@ export default function Home() {
       <HeroSection />
       <TypedSection texts={texts} period={period} beforeText={beforeText} />
       <ThoughtSlider />
-      <section>
-        <h1>Section 1</h1>
-        <h1>Section 2</h1>
-        <h1>Section 3</h1>
-        <h1>Section 4</h1>
-        <h1>Section 5</h1>
-      </section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="imgBox">
+              <Image src="/home/bg.png" width={250} height={250} alt="" />
+              <div className="innerWrap">
+              <Image src="/home/12.png" width={250} height={250} className="reviewIcon leftTop" alt="" />
+                <Image src="/home/12.png" width={250} height={250} className="profileImage" alt="" />
+              <Image src="/home/12.png" width={250} height={250} className="reviewIcon rightBottom" alt="" />
+              </div>
+              <Image src="/home/bg.png" width={250} height={250} alt="" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="contwentWrap">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consectetur, vero amet illum distinctio tempora sed ipsa natus laudantium voluptas in, corporis numquam vitae accusantium incidunt neque dicta impedit eligendi.</p>
+              <h2>Profile Title</h2>
+              <p>Designation</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </main>
   );
 }
